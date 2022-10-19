5a9a2f1f25e9b2961d49ea4c670aa57e9de84965
CebuCoder <55612034+Cebucoder@users.noreply.github.com>
2022-10-18 20:41:01 -0800

    Add files via upload

---
+++ Login/pass.js
@@ -0,0 +1,10 @@
+
+		function myFunction() {
+			var x = document.getElementById("myPassword");
+			if(x.type ==="password") {
+				x.type = "text";
+
+			}else {
+				x.type = "password" 
+			}
+		}
